52c28898719935b115ed2afcfc4fc193fd893a33
Navtoj Chahal <github@navtoj.com>
2024-09-04 18:45:40 -0700

    Initial Commit

---
+++ NotchBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NotchBar
+//
+//  Created by Navtoj Chahal on 2024-09-04.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
