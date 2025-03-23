d3738b6b6ef8594baa0adcfdfabab7e4dcf7a020
Navtoj Chahal <github@navtoj.com>
2025-03-23 15:07:20 -0700

    Track System Theme

--- tauri/src/routes/+layout.svelte
+++ tauri/src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+
 	let { children } = $props();
 </script>
+
+<ModeWatcher themeColors={{ dark: 'black', light: 'white' }} />
 
 {@render children()}
