f68bbd6d5e1da2ff9a53feddd6608862252e7955
Navtoj Chahal <github@navtoj.com>
2025-03-25 11:29:17 -0700

    qol: auto launch devtools

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -1,6 +1,14 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    use tauri::Manager;
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            if let Some(window) = app.get_webview_window("main") {
+                window.open_devtools();
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![check])
         .run(tauri::generate_context!())
