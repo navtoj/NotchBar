2ff4f40c96d11eeb32120337cd815360b891fd2b
Navtoj Chahal <github@navtoj.com>
2025-08-07 04:52:26 -0700

    Configure Manifest

--- Project.swift
+++ Project.swift
@@ -2,16 +2,38 @@ import ProjectDescription
 
 let project = Project(
 	name: "Placeholder",
+	settings: .settings(base: [
+		"ENABLE_USER_SCRIPT_SANDBOXING": true,
+		"ASSETCATALOG_COMPILER_GENERATE_SWIFT_ASSET_SYMBOL_EXTENSIONS": true,
+	]),
 	targets: [
 		.target(
 			name: "Placeholder",
 			destinations: .macOS,
 			product: .app,
 			bundleId: "dev.tuist.Placeholder",
-			infoPlist: .default,
+			deploymentTargets: .macOS("14.7.6"),
+			infoPlist: .extendingDefault(with: [
+				"CFBundleVersion": "1", // Internal
+				"CFBundleShortVersionString": "0.0.1", // Public
+				"LSApplicationCategoryType": "public.app-category.productivity",
+			]),
 			sources: ["App/Sources/**"],
 			resources: ["App/Resources/**"],
-			dependencies: []
+			entitlements: .dictionary([
+				"com.apple.security.app-sandbox": true,
+				"com.apple.security.files.user-selected.read-only": true,
+			]),
+			dependencies: [],
+			settings: .settings(base: [
+				"CODE_SIGN_STYLE": "Automatic", // Manual
+				"CODE_SIGN_IDENTITY": "Apple Development", // Mac Developer
+				"DEVELOPMENT_TEAM": "FUG9F8QSPW", // grep -r DEVELOPMENT_TEAM *
+				"ENABLE_HARDENED_RUNTIME": true,
+			]),
+			environmentVariables: [
+				"IDEPreferLogStreaming": .environmentVariable(value: "YES", isEnabled: true),
+			]
 		),
 	]
 )
