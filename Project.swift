bd77671f203987cf70692c3a0a77fa045dba4dbb
Navtoj Chahal <github@navtoj.com>
2025-08-20 13:27:47 -0700

    Add LaunchAtLogin

--- Project.swift
+++ Project.swift
@@ -27,6 +27,7 @@ let project = Project(
 			]),
 			dependencies: [
 				.external(name: "SFSafeSymbols"),
+				.external(name: "LaunchAtLogin"),
 			],
 			settings: .settings(base: [
 				"CODE_SIGN_STYLE": "Automatic", // Manual
