bd20e33928263371cccf40497ca66319de608dde
Navtoj Chahal <github@navtoj.com>
2025-08-20 09:10:04 -0700

    Replace Placeholder

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ import PackageDescription
 #endif
 
 let package = Package(
-	name: "Placeholder",
+	name: "NotchBar",
 	dependencies: [
 		// Add your own dependencies here:
 		// .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
