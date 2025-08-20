556f41c83e3f421a4e7c0867b02463490a277d16
Navtoj Chahal <github@navtoj.com>
2025-08-20 09:10:04 -0700

    Replace Placeholder

--- Project.swift
+++ Project.swift
@@ -1,22 +1,23 @@
 import ProjectDescription
 
 let project = Project(
-	name: "Placeholder",
+	name: "NotchBar",
 	settings: .settings(base: [
 		"ENABLE_USER_SCRIPT_SANDBOXING": true,
 		"ASSETCATALOG_COMPILER_GENERATE_SWIFT_ASSET_SYMBOL_EXTENSIONS": true,
 	]),
 	targets: [
 		.target(
-			name: "Placeholder",
+			name: "NotchBar",
 			destinations: .macOS,
 			product: .app,
-			bundleId: "dev.tuist.Placeholder",
+			bundleId: "com.navtoj.NotchBar",
 			deploymentTargets: .macOS("14.7.6"),
 			infoPlist: .extendingDefault(with: [
 				"CFBundleVersion": "1", // Internal
 				"CFBundleShortVersionString": "0.0.1", // Public
 				"LSApplicationCategoryType": "public.app-category.productivity",
+				"NSHumanReadableCopyright": "Copyright © Navtoj Chahal",
 			]),
 			sources: ["App/Sources/**"],
 			resources: ["App/Resources/**"],
